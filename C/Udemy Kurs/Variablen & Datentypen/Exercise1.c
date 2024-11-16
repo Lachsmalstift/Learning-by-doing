52e42dde965a4acab2f72eb13875e1f277dc639e
Lachsmalstift <123265267+Lachsmalstift@users.noreply.github.com>
2024-11-17 00:01:32 +0100

    nochmal gelernt

---
+++ C/Udemy Kurs/Variablen & Datentypen/Exercise1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() 
+{
+    int alter;
+    long long tage;
+    long long stu;
+    long long min;
+    long long sec;
+
+    printf("Hinweis: Schaltjahre sind nicht mit eingerechnet!\n");
+    printf("Gebe dein Alter in Jahren ein: ");
+    scanf("%i", &alter);
+
+    tage = (long long)alter * 365;
+    printf("Das sind %lld Tage\n", tage);
+
+    stu = tage * 24;
+    printf("%lld Stunden\n", stu);
+    
+    min = stu * 60;
+    printf("%lld Minuten\n", min);
+
+    sec = min * 60;
+    printf("%lld Sekunden\n", sec);
+
+    return 0;
+}
